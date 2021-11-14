c2bf3d0f1801ab4bb9acd06580b32f98fd14bc6e
KITTISAK <kittisak.sridet@icloud.com>
2021-11-14 16:20:33 +0700

    update

--- app/src/main/java/com/example/homeisolation/FirstFragment.kt
+++ app/src/main/java/com/example/homeisolation/FirstFragment.kt
@@ -1,52 +1,59 @@
 package com.example.homeisolation
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+import android.widget.*
+import android.widget.TextView
+
+
+
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+lateinit var txtUsername:TextView
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FirstFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FirstFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
+    private var mAuth: FirebaseAuth? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+        mAuth = FirebaseAuth.getInstance()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view =  inflater.inflate(R.layout.fragment_first, container, false)
+        var user: FirebaseUser? = mAuth!!.currentUser
+        txtUsername = view.findViewById<TextView>(R.id.txtUsername) as TextView
+        if(user != null){
+            txtUsername.setText("Hello "+user.email)
+            Log.d("FirstFragment", "User Sucess!"+user.email)
+        }
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+
+        return view
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FirstFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             FirstFragment().apply {
@@ -56,4 +63,5 @@ class FirstFragment : Fragment() {
                 }
             }
     }
+
 }
--- app/src/main/java/com/example/homeisolation/ListActivity.kt
+++ app/src/main/java/com/example/homeisolation/ListActivity.kt
@@ -5,6 +5,7 @@ import android.media.AudioPlaybackCaptureConfiguration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.*
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
@@ -22,51 +23,50 @@ class ListActivity : AppCompatActivity() {
     lateinit var navController: NavController
     lateinit var drawerLayout: DrawerLayout
     lateinit var navHostFragment: NavHostFragment
+    lateinit var navigationView: NavigationView
 
     private var mAuth: FirebaseAuth? = null
-    lateinit var buttonLogout: Button
-    lateinit var txtUsername:TextView
+
+    lateinit var textUsername:TextView
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
 
-        var toolbar = findViewById<Toolbar>(R.id.toolbar)
         navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
-
         drawerLayout = findViewById(R.id.drawer_layout)
+        appBarConfiguration = AppBarConfiguration(setOf(R.id.firstFragment,R.id.secondFragment),drawerLayout)
+        findViewById<Toolbar>(R.id.toolbar).setupWithNavController(navController,appBarConfiguration)
+        navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setupWithNavController(navController)
+        var header:View = navigationView.getHeaderView(0);
+        textUsername = header.findViewById(R.id.user_name)
 
-        appBarConfiguration = AppBarConfiguration(navController.graph,drawerLayout)
-        toolbar.setupWithNavController(navController,appBarConfiguration)
-
-        findViewById<NavigationView>(R.id.nav_view).setupWithNavController(navController)
-
-
-//        buttonLogout = findViewById<Button>(R.id.buttonLogout)
-//        txtUsername = findViewById<TextView>(R.id.user_name)
         mAuth = FirebaseAuth.getInstance()
 
 //        buttonLogout!!.setOnClickListener {
 //            logout()
 //        }
-        
+        Log.d("ListActivity onCreate", "User Sucess!")
+
+
     }
 
     override fun onStart() {
         super.onStart()
         var currentUser = mAuth!!.currentUser
         updateUI(currentUser)
+
     }
 
     private fun updateUI(user: FirebaseUser?) {
         if(user != null){
-//            txtUsername.setText(user.email)
+            Log.d("ListActivity", "User Sucess!"+user.email)
+            textUsername.setText(user.email)
             return
-        }else{
-            val intentSession  = Intent(this,MainActivity::class.java)
-            startActivity(intentSession)
         }
     }
 
